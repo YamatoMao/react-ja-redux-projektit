9096e5d5c19101784c2433d1d121d411fbfd56ab
Umanskii Daniil <s2001083@edu.bc.fi>
2022-01-25 13:37:29 +0200

    Person lisätty

--- 02-task/src/App.js
+++ 02-task/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './person/Person.js'
+import Person from './person/Person.js';
 
 const App = () => {
   return (
     <div className="App">
       <h1>React app putsattu!</h1>
+      <Person />
     </div>
   );
 }
---
+++ 02-task/src/person/Person.js
@@ -0,0 +1,11 @@
+import './Person.css';
+
+const Person = () => {
+  return (
+    <div className="Person">
+      <p class="punainen">Hello, my name is your_name, and I am your_age old.</p>
+    </div>
+  );
+}
+
+export default Person;
