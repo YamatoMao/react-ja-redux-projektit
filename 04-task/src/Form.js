fc9fbd4257cdfb2e639b06d31414eb7b357ec0e8
YamatoMao <umanskiydanya@gmail.com>
2022-02-08 12:33:47 +0200

    04-task valmis

--- 04-task/src/Form.js
+++ 04-task/src/Form.js
@@ -1,30 +1,65 @@
 import './Form.css';
 import View from './View.js';
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [phone, setPhone] = useState();
+    const [message, setMessage] = useState();
+    
   return (
-    <section>
+    <section id="formi">
     <form className="Form">
     <h1>Fill the Form</h1>
         <div className="mb-3">
-            <label for="firstname" className="fomr-label">First name:</label>
-            <input id="firstname" type="text" className="form-control" aria-describedby="firstname"></input>
+            <label for="firstname" className="form-label">First name:</label>
+            <input id="firstname" 
+            type="text" 
+            className="form-control" 
+            aria-describedby="firstname" 
+            name="firstname"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            />
         </div><br />
         <div className="mb-4">
-            <label for="lastname" className="fomr-label">Last name:</label>
-            <input id="lastname" type="text" className="form-control" aria-describedby="lastname"></input>
+            <label for="lastname" className="form-label">Last name:</label>
+            <input id="lastname" 
+            type="text" 
+            className="form-control" 
+            aria-describedby="lastname"
+            name="lastname"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            />
         </div><br />
         <div className="mb-5">
-            <label for="phone" className="fomr-label">Phone:</label>
-            <input id="phone" type="text" className="form-control" aria-describedby="phone"></input>
+            <label for="phone" className="form-label">Phone:</label>
+            <input id="phone" 
+            type="text" 
+            className="form-control" 
+            aria-describedby="phone"
+            name="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            />
         </div><br />
         <div className="mb-6">
-            <label for="message" className="fomr-label">Message:</label>
-            <input id="message" type="text" className="form-control" aria-describedby="message"></input>
+            <label for="message" className="form-label">Message:</label>
+            <input id="message" 
+            type="text" 
+            className="form-control" 
+            aria-describedby="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            />
         </div>
     </form>
-    <View />
+    <br />
+    <View firstName={firstName} lastName={lastName} phone={phone} message={message} />
     </section>
   );
 }
--- 04-task/src/View.js
+++ 04-task/src/View.js
@@ -1,9 +1,14 @@
 import './View.css';
+import Form from "./Form";
 
-const View = () => {
+const View = (props) => {
   return (
       <section className="View">
-          <p>Mmm</p>
+          <h2>This is your input: </h2>
+          <p><span>First name: </span>{props.firstName}</p>
+          <p><span>Last name: </span>{props.lastName}</p>
+          <p><span>Phone: </span>{props.phone}</p>
+          <p><span>Message: </span>{props.message}</p>
       </section>
   );
 }
